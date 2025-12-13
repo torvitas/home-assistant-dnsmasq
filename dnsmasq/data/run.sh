b70be820ce015e8a794ebb523cc6b11f921cefdd
Schmidt, Sascha (sasschmidt) <sascha.schmidt@breuninger.de>
2025-12-13 21:42:12 +0100

    fix: Add X86 boot files

--- dnsmasq/data/run.sh
+++ dnsmasq/data/run.sh
@@ -2,9 +2,6 @@
 # shellcheck shell=bash
 set -e
 
-mkdir -p /etc/dnsmasq.d
-echo $(bashio::config "config") >/etc/dnsmasq.d/pxe.conf
-
 bashio::log.info "Starting dnsmasq..."
 
 exec /usr/sbin/dnsmasq $(bashio::config "arguments")
