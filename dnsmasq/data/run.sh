d15101fb072f072cdd1e947d7090784afd6914bf
Schmidt, Sascha (sasschmidt) <sascha.schmidt@breuninger.de>
2025-12-13 21:00:03 +0100

    feat: Add configuration option

--- dnsmasq/data/run.sh
+++ dnsmasq/data/run.sh
@@ -2,6 +2,9 @@
 # shellcheck shell=bash
 set -e
 
+mkdir -p /etc/dnsmasq.d
+echo $(bashio::config "config") >/etc/dnsmasq.d/pxe.conf
+
 bashio::log.info "Starting dnsmasq..."
 
 exec /usr/sbin/dnsmasq $(bashio::config "arguments")
